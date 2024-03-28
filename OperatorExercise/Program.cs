69bc9eee91ca246bfb004df4909124a0f06d6491
Guillermo Ramirez <r.dannysixty@gmail.com>
2024-03-28 16:09:28 -0700

    tried to fix

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -18,18 +18,18 @@ namespace OperatorsInClass
             return answer;
         }
 
-        public static int times(int num1, int num2)
+        public static int Times(int num1, int num2)
         {
             var answer = num1 * num2;
             return answer;
 
         }
-        public static int divide(int num1, int num2)
+        public static int Divide(int num1, int num2)
         {
             var answer = num1 / num2;
             return answer;
         }
-        static void main(string[] args)
+        static void Main(string[] args)
         {
             var a = 17;
             var b = 4;
@@ -37,12 +37,12 @@ namespace OperatorsInClass
             var remainder = a % b;
             Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
 
-
+            Console.WriteLine("Please enter radius of your circle");
             var userInput = Console.ReadLine();
 
             var radius = double.Parse(userInput);
 
-            Console.WriteLine(AreaOfCircle(radius));
+           Console.WriteLine(AreaOfCircle(radius));
         }
 
         public static double AreaOfCircle(double radius)
--- OperatorExercise/obj/Debug/net6.0/OperatorExercise.AssemblyInfo.cs
+++ OperatorExercise/obj/Debug/net6.0/OperatorExercise.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OperatorExercise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d2d1b8f262369534f5740c99b29542f2d33f8b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5dd7928576178ddc520c3df122dd6aa086b7cfa")]
 [assembly: System.Reflection.AssemblyProductAttribute("OperatorExercise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OperatorExercise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
