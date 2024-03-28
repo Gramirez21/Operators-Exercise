f5dd7928576178ddc520c3df122dd6aa086b7cfa
Guillermo Ramirez <r.dannysixty@gmail.com>
2024-03-28 12:44:12 -0700

    rewrote code

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,10 +1,57 @@
-﻿namespace OperatorExercise
+﻿using System;
+using System.ComponentModel;
+
+namespace OperatorsInClass
 {
-    public class Program
+    class program
+
     {
-        static void Main(string[] args)
+        public static int Add(int num1, int num2)
         {
-            Console.WriteLine("Hello, World!");
+            var answer = num1 + num2;
+            return answer;
+        }
+
+        public static int Subtract(int num1, int num2)
+        {
+            var answer = num1 - num2;
+            return answer;
+        }
+
+        public static int times(int num1, int num2)
+        {
+            var answer = num1 * num2;
+            return answer;
+
+        }
+        public static int divide(int num1, int num2)
+        {
+            var answer = num1 / num2;
+            return answer;
+        }
+        static void main(string[] args)
+        {
+            var a = 17;
+            var b = 4;
+            var quotient = a / b;
+            var remainder = a % b;
+            Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+
+
+            var userInput = Console.ReadLine();
+
+            var radius = double.Parse(userInput);
+
+            Console.WriteLine(AreaOfCircle(radius));
+        }
+
+        public static double AreaOfCircle(double radius)
+        {
+            var area = Math.PI * Math.Pow(radius, 2);
+            return area;
         }
     }
+
+
 }
+
